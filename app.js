220fc2a0400fbbd78adf5a2bd02d8c86493a0cf0
andrew-kyle <56801661+andrew-kyle@users.noreply.github.com>
2020-06-06 17:16:10 -0700

    added more features

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https');
 const app = express();
 
 app.use(bodyParser.json());
@@ -10,25 +11,68 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static("public"));
 
 app.get("/", function(req,res){
-    res.sendfile(__dirname + "/signup.html");
+    res.sendFile(__dirname + "/signup.html");
 });
 
 app.post("/", function(req,res){
-    console.log("This works");
 
-    let firstName = req.body.firstName;
-    let lastName = req.body.lastName;
-    let email = req.body.email
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email
 
-    res.write("First name: " + firstName);
-    res.write("\nLast name: " + lastName);
-    res.write("\nEmail: " + email);
-    res.send();
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    const url = 'https://us10.api.mailchimp.com/3.0/lists/1e3be4cf19';
+
+    const options = {
+        method: "POST",
+        auth: "andrewkyle92:053a015a9015d43209b75555b2053265-us10"
+    }
+
+    const request = https.request(url, options, function(response){
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure", function(req,res){
+    res.redirect("/");
+})
+
+
+
+
+app.listen(3004, function(){
+    console.log("Server running on port 3004.");
 });
 
 
+//API Key
+//053a015a9015d43209b75555b2053265-us10
 
-
-app.listen(3002, function(){
-    console.log("Server running on port 3002.");
-});
+//listID
+//1e3be4cf19
